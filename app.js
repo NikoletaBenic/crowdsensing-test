c8a6c6bb6b6495ec20161f62c20327dc285ecd87
Nikoleta Benić <nb53112@fer.hr>
2023-05-22 13:07:43 +0200

    addMesaurement

--- app.js
+++ app.js
@@ -10,6 +10,39 @@ app.get('/', (req, res) => {
   res.send('Server side is running')
 })
 
+app.post("/addMeasurement", [
+  check("location").notEmpty().withMessage("Location must be provided!"),
+  check("location.lat").notEmpty().isNumeric().withMessage("Invalid location input!"),
+  check("location.lng").notEmpty().isNumeric().withMessage("Invalid location input!"),
+  check("datetime").notEmpty().withMessage("Please specify time and date for the measurement!")
+], async(req,res) => {
+
+  const { temperature, noiseLevel, ambientLight, pressure } = req.body;
+  const nonEmptyFields = [temperature, noiseLevel, ambientLight, pressure].filter(field => field !== undefined && field !== '' && field !== null);
+  
+  if (nonEmptyFields.length < 1) {
+      return res.status(400).json({ error: "At least one measurement field must be provided!" });
+  }
+
+  if (!nonEmptyFields.every(field => !isNaN(parseFloat(field)))) {
+      return res.status(400).json({ error: "Invalid measurement provided!" });
+    }
+
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+      console.log(errors);
+      return res.status(400).json({errors : errors.array()});
+  } else {
+      const data = req.body;
+  await Measurements.add(data);
+  res.send({msg:"Measurement added"});
+  console.log(data);
+  }
+  
+});
+
+
+
 app.get("/getMeasurements", async (req, res) => {
   const snapshot = await Measurements.get();
   const list = snapshot.docs.map((doc)=>({id: doc.id, ...doc.data()}));
