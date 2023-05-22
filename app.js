ac9c827a4111a81f310bd6b0ccf717e8007ae30d
Nikoleta Benić <nb53112@fer.hr>
2023-05-22 12:46:35 +0200

    test get

--- app.js
+++ app.js
@@ -2,11 +2,23 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const Measurements = require('./config')
+const {check, validationResult} = require("express-validator");
+app.use(express.json());
+
 
 app.get('/', (req, res) => {
   res.send('Server side is running')
 })
 
+app.get("/getMeasurements", async (req, res) => {
+  //const snapshot = await Measurements.get();
+  //const list = snapshot.docs.map((doc)=>({id: doc.id, ...doc.data()}));
+  res.send("works");
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
